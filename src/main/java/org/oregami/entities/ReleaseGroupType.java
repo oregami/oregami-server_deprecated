90d203e10ceb1f41c3f1a1ce0935000b48b6bf37
Tracy Poff <tracy.poff@gmail.com>
2013-11-16 16:23:43 -0500

    Removed inaccurate copyright header.

--- src/main/java/org/oregami/entities/ReleaseGroupType.java
+++ src/main/java/org/oregami/entities/ReleaseGroupType.java
@@ -1,19 +1,3 @@
-/*******************************************************************************
- * Copyright (C) 2012  Oregami.org, Germany http://www.oregami.org
- * 
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of version 3 or any later version of the
- *  GNU Affero General Public License as published by the Free Software 
- *  Foundation.
- *  
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Affero General Public License for more details. 
- *  
- *  You should have received a copy of the GNU Affero General Public License
- *  along with this program. If not, see <http://www.gnu.org/licenses/>.
- ******************************************************************************/
 package org.oregami.entities;
 
 import javax.persistence.Entity;
